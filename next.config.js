2cbccea87e4173a7778109307759420be24701c7
Nekoshita Yuki <yuki.303442@gmail.com>
2021-03-03 10:02:12 +0900

    fix: nextjs config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const withPlugins = require("next-compose-plugins");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages();
-
-module.exports = {
-  env: {
-    ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
+module.exports = withPlugins([
+  withOptimizedImages(),
+  {
+    env: {
+      ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
+    },
   },
-};
+]);
